babd2b7cde3efda643c8ac8659934125eb62b1f5
Rick-Mello <rikelmemelo16@gmail.com>
2025-11-12 10:50:01 -0300

    Update banco_de_dados.py

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -10,14 +10,17 @@
         "email": "gabriel123@gmail.com",
         "senha": "gabriel123",
         "ra": "202540580",
-        "nota1_Engenharia de software ágil": "10",
+        "nota1_Engenharia de software ágil": "5",
         "nota2_Engenharia de software ágil": "10",
-        "faltas_Engenharia de software ágil": "16"
+        "faltas_Engenharia de software ágil": "3"
     },
     {
         "nome": "lucas",
         "email": "lucas@gmail.com",
         "senha": "12345",
-        "ra": "202518409"
+        "ra": "202518409",
+        "nota1_Algoritmo e estrutura de dados python": "10",
+        "nota2_Algoritmo e estrutura de dados python": "9",
+        "faltas_Algoritmo e estrutura de dados python": "3"
     }
 ]
